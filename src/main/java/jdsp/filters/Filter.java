90513e3b736e2bb3039786441bdc2903a127149a
Keith Chow <kchow@localhost.localdomain>
2019-05-27 11:51:21 -0400

    Added convolve method and test

--- src/main/java/jdsp/filters/Filter.java
+++ src/main/java/jdsp/filters/Filter.java
@@ -1,4 +1,46 @@
 package jdsp.filters;
-class Filter{
+/**
+ * The Filter class will implement the following static methods:
+ *      convolve()
+ */
+public class Filter{
+    
+    public static float[] convolve(float[] input1, float[] input2){
+        int len1 = input1.length;
+        int len2 = input2.length;
+        int len3 = len1 + len2 - 1;
+        int minLen = (len1<len2)?len1:len2;
+        int start, end;
+        float[] output = new float[len3];
+        if (len2 >= len1){
+            for (int ind0 = 0; ind0 < len1; ind0++){
+                for (int ind1 = 0; ind1 < ind0+1; ind1++){
+                    output[ind0] += input1[ind0 - ind1] * input2[ind1];
+                }
+            }
+            for (int ind0 = len1; ind0 < len3; ind0++){
+                start = (ind0-len2+1<0) ? 0 : ind0 - len2 + 1;
+                for (int ind1=start; ind1 < len1; ind1++){
+                    float tmp = input2[ind0-ind1];
+                    output[ind0] += input1[ind1] * tmp;
+                }
+            }
+        }
+        else{
+            for (int ind0 = 0; ind0 < len2; ind0++){
+                for (int ind1 = 0; ind1 < ind0 + 1; ind1++){
+                    output[ind0] += input2[ind0 - ind1] * input1[ind1];
+                }
+            }
+            for (int ind0 = len2; ind0 < len3; ind0++){
+                start = (ind0 - len1 + 1 < 0) ? 0 : ind0 - len1 + 1;
+                for (int ind1=start; ind1 < len2; ind1++){
+                    float tmp = input1[ind0-ind1];
+                    output[ind0] += input2[ind1] * tmp;
+                }
+            }
+        }
 
+        return output;
+    }
 }
---
+++ src/test/java/TestFilters.java
@@ -0,0 +1,62 @@
+
+import jdsp.filters.Filter;
+public class TestFilters{
+    public static void compareVectors(float[] vector1, float[] vector2){
+        if (vector1.length != vector2.length){
+            throw new AssertionError("Lengths do not match");
+        }
+        boolean passed = true;
+        for (int ind0=0; ind0<vector1.length; ind0++){
+            if (vector1[ind0] != vector2[ind0]){
+                //throw new AssertionError("Element " + 
+                //        Float.toString(ind0) + " does not match");
+                //System.out.println("Failed on index = " + Float.toString(ind0));
+                //System.out.println("\t" + vector1[ind0] + "\tvs\t" + vector2[ind0]);
+                passed = false;
+            }
+        }
+        if (!passed){
+            for (int ind0=0; ind0<vector1.length; ind0++){
+                System.out.print(vector1[ind0] + ",");
+            }
+            System.out.println();
+            for (int ind0=0; ind0<vector1.length; ind0++){
+                System.out.print(vector2[ind0] + ",");
+            }
+            System.out.println();
+        }        
+    }
+
+    public static void testConv(){
+        float[] expected1 = {1.0f, 4.0f, 4.0f};
+        float[] expected2 = {9.0f, 24.0f, 46.0f, 40.0f, 25.0f};
+        float[] expected3 = {3.0f, 10.0f, 13.0f, 10.0f};
+        float[] expected4 = {3.0f, 16.0f, 33.0f, 36.0f, 20.0f};
+        float[] s1 = {1.0f, 2.0f};
+        float[] s2 = {3.0f, 4.0f, 5.0f};
+        float[] s3;
+
+        // expecting {1, 4, 4}
+        s3 = Filter.convolve(s1, s1);
+        compareVectors(s3, expected1);
+        System.out.println("Conv test1 passed");
+
+        // expecting {9, 24, 46, 40, 25}
+        s3 = Filter.convolve(s2, s2);
+        compareVectors(s3, expected2);
+        System.out.println("Conv test2 passed");
+
+        // expecting {3, 10, 13, 10}
+        s3 = Filter.convolve(s2, s1);
+        compareVectors(s3, expected3);
+        System.out.println("Conv test3 passed");
+
+        s3 = Filter.convolve(expected3, s1);
+        compareVectors(s3, expected4);
+        System.out.println("Conv test4 passed");
+    }
+    public static void main(String[] args){
+        testConv();
+
+    }
+}
